5d9fd35ccb86a596a379ee60dc07d4359dfaf860
Toon Alfrink <toon.alfrink@gmail.com>
2013-12-06 11:02:38 +0100

    updated maintenance scripts

--- maintenance/daily.sh
+++ maintenance/daily.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 DATE=$(date -d 'yesterday' +'%Y-%m-%d')
-cd $PYTHONPATH
-hg up -C trunk
-cd $PYTHONPATH/scrapers
-hg up -C default
-wait
-python $PYTHONPATH/amcat/scripts/maintenance/daily.py $DATE --deduplicate True 2>&1
+python $PYTHONPATH/amcat/scripts/maintenance/daily.py $DATE
 wait
 python $PYTHONPATH/amcat/scripts/maintenance/scraping_check.py $DATE $SCRAPING_CHECK_MAIL
--- maintenance/hourly.sh
+++ maintenance/hourly.sh
@@ -1,11 +1,7 @@
 #!/bin/bash                                                                                          
 
-DATE=$(date +'%Y-%m-%d')
 python $PYTHONPATH/scrapers/tv/teletekst.py $TELETEKST_PROJECT --articleset $TELETEKST_ARTICLESET
-wait
-DEDUPLICATE='python '$PYTHONPATH'/amcat/scripts/maintenance/deduplicate.py'
-$DEDUPLICATE $TELETEKST_ARTICLESET --first_date $DATE --last_date $DATE
 wait
 kill `ps -ef |grep twitter_statuses_filter|grep -v grep|awk '{print $2}'`
 wait
-python $PYTHONPATH/scrapers/social/twitter/twitter_statuses_filter.py $DATE
+python $PYTHONPATH/scrapers/social/twitter/twitter_statuses_filter.py
