712707d50734b852314c26f9665cc8cd0396f5f3
Toon Alfrink <toon.alfrink@gmail.com>
2014-06-06 13:45:26 +0200

    remade telegraaf scraper

--- newspapers/telegraaf.py
+++ newspapers/telegraaf.py
@@ -19,122 +19,69 @@ from __future__ import unicode_literals, print_function, absolute_import
 # License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
 ###########################################################################
 
-from amcat.scraping.scraper import HTTPScraper, DBScraper
-from amcat.scraping.document import HTMLDocument
-
-from amcat.scraping import toolkit
-from amcat.scraping.toolkit import parse_coord
-
-
-import logging; log = logging.getLogger("amcat.scraping.scraper")
-
-INDEX_URL = "https://telegraaf-i.telegraaf.nl/telegraaf/_main_/%(year)d/%(month)02d/%(day)02d/001"
-LOGIN_URL = "https://telegraaf-i.telegraaf.nl/telegraaf/_main_/{y}/{m:02d}/{d:02d}/001/page.html"
-other_url = "https://telegraaf-i.telegraaf.nl/tmg/login.php"
 from urllib import urlencode
 from urlparse import urljoin
+import json, re
 
-CREDENTIALS_ERR = "Login page returned code %s. Wrong credentials?"
+from amcat.scraping.scraper import HTTPScraper, DBScraper
+from amcat.scraping.toolkit import parse_form
 
 class TelegraafScraper(HTTPScraper, DBScraper):
     medium_name = "De Telegraaf"
-    pagenr = 0
+    week_url = "http://www.telegraaf.nl/telegraaf-i/week"
 
     def _login(self, username, password):
-        l_url = LOGIN_URL.format(
-                y=self.options['date'].year,
-                m=self.options['date'].month,
-                d=self.options['date'].day)
-        pagel = self.getdoc(l_url)
-        form = toolkit.parse_form(pagel)
-        form["sso:field:username"] = username
-        form["sso:field:password"] = password
-        pagel = self.open(other_url,urlencode(form))
-
-        if pagel.getcode() != 200:
-            raise ValueError(CREDENTIALS_ERR % pagel.getcode())
-
+        login_url = "http://www.telegraaf.nl/wuz/loginbox/epaper?nocache"
+        self.open(self.week_url)
+        form = parse_form(self.getdoc(login_url).cssselect("#user-login")[0])
+        form['name'], form['pass'] = username, password
+        form['rhash'] = "f8ac71adde5cdb382ab5e485a8c3447210a6b69b"
+        form['redir'] = self.week_url
+        self.opener.opener.addheaders += [("Host" , "www.telegraaf.nl"), ("Referer", login_url)]
+        res = self.open(login_url, urlencode(form))
+        if not "user_name" in str(self.opener.cookiejar):
+            raise ValueError("wrong user/pass")
 
     def _get_units(self):
-        """
-        @type date: datetime.date, datetime.datetime
-        @param date: date to scrape for.
-        """
-        date = self.options['date']
-        index = INDEX_URL % dict(year=date.year, month=date.month, day=date.day)
-        doc = self.getdoc(index)
-        self.categories = self.get_categories(doc)
-        for td in doc.cssselect('td.select_page option'):
-            url = urljoin(index, td.get('value') + '/page.html')
-            yield url
+        d = self.options['date']
+        data = json.loads(self.open("http://www.telegraaf.nl/telegraaf-i/newspapers").read())
+        self.paperdata = [i for i in data if i['date'] == "{}-{:02d}-{:02d}".format(d.year,d.month,d.day)][0]
+        articles = []
+        for page in self.paperdata['pages']:
+            articles += page['articles']
+        for article_id in articles:
+            yield article_id
 
-    def get_categories(self, doc):
-        borders = {}
-        for a in doc.cssselect("td.nav tr")[1].cssselect("a"):
-            try:
-                pagenr = int(a.get('href').split("/")[-1])
-            except AttributeError:
-                #some html fail
-                continue
-            cat = a.text_content()
-            borders[pagenr] = cat
+    def _scrape_unit(self, article_id):
+        url = "http://www.telegraaf.nl/telegraaf-i/article/{}".format(article_id)
+        article = {'url' : url, 'metastring' : {}, 'externalid' : article_id, 'children' : []}
+        data = json.loads(self.open(url).read())
+        lead, text = "", ""
+        for d in data['body']:
+            for k, v in d.items():
+                if k == 'lead':
+                    lead += v + "\n\n"
+                    if re.match('[A-Z ]+, [a-z]+', v):
+                        article['metastring']['dateline'] = v
+                elif k == 'headline':
+                    article['headline'] = v
+                elif k == 'paragraph':
+                    text += v + "\n\n"
 
-        categories = {}
-        cat = borders[1]
-        for td in doc.cssselect('td.select_page option'):
-            pagenr = int(td.get('value'))
-            if pagenr in borders.keys():
-                cat = borders[pagenr]
+        article['text'] = lead + "\n\n" + text
+        for page in self.paperdata['pages']:
+            if article_id in page['articles']:
+                article['pagenr'] = int(page['page_number'])
                 
-            categories[pagenr] = cat
+        for section in self.paperdata['sections']:
+            if article['pagenr'] in map(int, section['pages']):
+                article['section'] = section['title']
+        article['byline'] = "\n".join(data['byline'])
 
-        return categories
-            
-        
-
-    def _scrape_unit(self, url):
-        doc = self.getdoc(url)
-        # Articles with an id higher than 100 are advertisements,
-        # which can be filtered by excluding classnames lager than
-        # 9 (articleXXX).
-        articles = doc.cssselect('#page div')
-        articles = set(div.get('class') for div in articles
-                            if len(div.get('class')) <= 9)
-
-        for clsname in articles:
-            page = HTMLDocument()
-
-            # Delete images
-            for img in doc.cssselect('#article img'):
-                img.delete_tree()
-
-            divs = doc.cssselect('div.%s' % clsname)
-            page.props.url = urljoin(url,
-                                     "article/%s.html" % clsname[7:])
-            page.prepare(self)
-            page.props.pagenr = int(url.split("/")[-2])
-            page.props.section = self.categories[page.props.pagenr]
-            page.props.text = page.doc.cssselect('#article .body p')
-            page.props.date = self.options['date']
-            if page.doc.cssselect("#article p.dateline"):
-                page.props.dateline = page.doc.cssselect("#article p.dateline")[0].text_content()
-            if page.doc.cssselect("#article .kicker"):
-                page.props.kicker = page.doc.cssselect("#article .kicker")[0].text_content()
-            if page.doc.cssselect("#article address"):
-                page.props.author = page.doc.cssselect("#article address")[0].text_content().strip().lstrip("dor").strip()
-            if page.doc.cssselect('#article h1'):
-                page.props.headline = page.doc.cssselect('#article h1')[0].text_content()
-                all_text = "".join([p.text_content() for p in page.props.text])
-                if len(all_text) < 300:
-                    continue
-                yield page
-
-
-
+        if not article['section'] == "Advertentie":
+            print(article)
+            yield article
 
 if __name__ == '__main__':
     from amcat.scripts.tools import cli
-    from amcat.tools import amcatlogging
-    amcatlogging.debug_module("amcat.scraping.scraper")
-    amcatlogging.debug_module("amcat.scraping.document")
     cli.run_cli(TelegraafScraper)
